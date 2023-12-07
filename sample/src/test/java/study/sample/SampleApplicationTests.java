b650c4cefbb2e33749684e42b7a1d471d1903723
ambosing <ambosing_@naver.com>
2023-12-07 21:33:37 +0900

    테스트 코드 기초 작성

---
+++ sample/src/test/java/study/sample/SampleApplicationTests.java
@@ -0,0 +1,12 @@
+package study.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
