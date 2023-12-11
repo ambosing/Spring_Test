c4cc54f2b13ea7550a4c452f34c16f326600a897
ambosing <ambosing_@naver.com>
2023-12-11 15:23:38 +0900

    userController 테스트 메서드 추가

---
+++ test-code-with-architecture-main/src/test/resources/sql/user-controller-test-data.sql
@@ -0,0 +1,4 @@
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (1, 'ambosing_@naver.com', 'ambosing', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa', 'ACTIVE', 0);
+insert into `users` (`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values (2, 'ambosing7@naver.com', 'ambosing7', 'Seoul', 'aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab', 'PENDING', 0);
