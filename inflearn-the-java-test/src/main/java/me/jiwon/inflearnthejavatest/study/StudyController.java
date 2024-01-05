e79dd885c9aed978a0c84782ff1edd2d96034829
ambosing <ambosing_@naver.com>
2024-01-05 16:12:22 +0900

    Jmeter 테스트를 위한 컨트롤러 작성

---
+++ inflearn-the-java-test/src/main/java/me/jiwon/inflearnthejavatest/study/StudyController.java
@@ -0,0 +1,24 @@
+package me.jiwon.inflearnthejavatest.study;
+
+import lombok.RequiredArgsConstructor;
+import me.jiwon.inflearnthejavatest.domain.Study;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+public class StudyController {
+
+    final StudyRepository repository;
+
+    @GetMapping("/study/{id}")
+    public Study getStudy(@PathVariable("id") Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Study not found for '" + id + "'"));
+    }
+
+    @PostMapping("/study")
+    public Study createsStudy(@RequestBody Study study) {
+        return repository.save(study);
+    }
+
+}
